49c123547a61f0c31e7f88fd9c526ad124d97843
Twilight-Mizukaze <65389377+Twilight-Mizukaze@users.noreply.github.com>
2021-03-05 22:20:34 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function kansu(){
+  alert('Hello');
+  var name = document.getElementById('btn').value;
+  var hello = document.getElementById('name');
+
+  hello.innerHTML = name + 'さん こんにちは';
+}
+function hello(){
+  alert('三年生');
+}

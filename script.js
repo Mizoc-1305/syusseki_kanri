f27afc85c5195937312f9f5105eacaee65c8661e
Kengokuma <kengo171227@gmail.com>
2021-03-23 16:28:01 +0900

    main

--- script.js
+++ script.js
@@ -244,7 +244,6 @@ function reload_tab() {
 }
 
 function add_member() {
-  alert('hello');
   var new_name = document.getElementById('form_name').value;
   var new_grade = document.getElementsByClassName('radio');
   for (var value = "", i = new_grade.length; i--;) {
@@ -531,7 +530,11 @@ function setting_export() {
   (new CSV(values)).save('setting.csv')
 }
 function setting_import() {
-
+  var csv_member = reader.result.split('\n')
+  var changed_array = []
+  for (let index = 0; index < csv_member.length; index++) {
+    changed_array[index] = csv_member[index].split(',')
+  }
 }
 function excel_input() {
   var first_index = ["項目"];
